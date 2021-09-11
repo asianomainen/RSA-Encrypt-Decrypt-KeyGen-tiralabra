77304c6542d2d281a41ba20e6aef15da0c946965
asianomainen <oskari.nuottonen@gmail.com>
2021-09-11 04:14:41 +0300

    luo maven-projektin

---
+++ RSAtool/src/main/java/rsatoolapp/Main.java
@@ -0,0 +1,9 @@
+package rsatoolapp;
+
+import rsatoolapp.ui.RSAtoolUi;
+
+public class Main {
+    public static void main(String[] args) {
+        RSAtoolUi.main(args);
+    }
+}
---
+++ RSAtool/src/main/java/rsatoolapp/ui/RSAtoolUi.java
@@ -0,0 +1,12 @@
+package rsatoolapp.ui;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class RSAtoolUi extends Application {
+
+    @Override
+    public void start(Stage stage) {
+
+    }
+}
