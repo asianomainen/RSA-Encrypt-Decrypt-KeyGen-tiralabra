252fff209ee39aecc37895aceecba24108f21ee2
asianomainen <oskari.nuottonen@gmail.com>
2021-10-30 19:25:48 +0300

    Lisää testejä KeyGenerator-luokalle

--- RSAtool/src/tests/java/rsatoolapp/KeyGeneratorTest.java
+++ RSAtool/src/tests/java/rsatoolapp/KeyGeneratorTest.java
@@ -5,12 +5,7 @@ import org.junit.Test;
 import rsatoolapp.domain.KeyGenerator;
 import rsatoolapp.domain.RSAKey;
 
-import javax.crypto.BadPaddingException;
-import javax.crypto.IllegalBlockSizeException;
-import javax.crypto.NoSuchPaddingException;
-import java.io.IOException;
-import java.security.*;
-import java.security.spec.InvalidKeySpecException;
+import java.math.BigInteger;
 
 import static org.junit.Assert.*;
 
@@ -18,7 +13,7 @@ public class KeyGeneratorTest {
     KeyGenerator kg;
 
     @Before
-    public void setUp() throws NoSuchAlgorithmException {
+    public void setUp() {
         kg = new KeyGenerator();
     }
     @Test
@@ -34,7 +29,7 @@ public class KeyGeneratorTest {
     }
 
     @Test
-    public void publicKeysDoNotMatchIfGeneratedAgain() throws IOException, InvalidKeySpecException, NoSuchPaddingException, IllegalBlockSizeException, NoSuchAlgorithmException, BadPaddingException, InvalidKeyException {
+    public void publicKeysDoNotMatchIfGeneratedAgain() {
         kg.generateKeys();
         RSAKey publicKey1 = kg.getPublicKey();
 
@@ -45,7 +40,7 @@ public class KeyGeneratorTest {
     }
 
     @Test
-    public void privateKeysDoNotMatchIfGeneratedAgain() throws IOException, InvalidKeySpecException, NoSuchPaddingException, IllegalBlockSizeException, NoSuchAlgorithmException, BadPaddingException, InvalidKeyException {
+    public void privateKeysDoNotMatchIfGeneratedAgain() {
         kg.generateKeys();
         RSAKey privateKey1 = kg.getPrivateKey();
 
@@ -53,5 +48,24 @@ public class KeyGeneratorTest {
         RSAKey privateKey2 = kg.getPrivateKey();
 
         assertNotEquals(privateKey1, privateKey2);
+    }
+
+    @Test
+    public void privateAndPublicKeysDoNotMatch() {
+        kg.generateKeys();
+        RSAKey publicKey = kg.getPublicKey();
+        RSAKey privateKey = kg.getPrivateKey();
+
+        assertNotEquals(publicKey, privateKey);
+    }
+
+    @Test
+    public void compositeNumberDoesNotPassMillerRabinTest() {
+        assertFalse(kg.millerRabin(BigInteger.TEN));
+    }
+
+    @Test
+    public void compositeNumberIsNotPrime() {
+        assertFalse(kg.isPrime(BigInteger.TEN));
     }
 }
